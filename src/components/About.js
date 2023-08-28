a011a8304049e5c9ea9a21bcf1824fb703dc532c
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-08-28 07:06:24 +0200

    adapt about section + minor changes to stage, skills and contact section

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,40 @@
 import React from "react";
 import tagBracketsAbout from "../assets/tag-brackets-about.svg";
 
+
 const About = () => {
   return (
     <section id="about" className="bg-mediumblue px-9 sm:px-16 py-32 text-white">
-      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+      <div className="container mx-auto grid lg:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
           <h2 className="text-3xl sm:text-4xl lg:text-5xl mb-8 sm:mb-12 text-tang">About me
           </h2>
 
-          <p className="pb-5 text-lg">
-            As a  <span className="text-tang">freshly trained full-stack web developer</span>, I find great joy in combining my previous
-            experience of over 6 years in political communications with my newfound
-            passion for web development.
+          <p className="text-tang text-lg mb-3 flex">I am a certified web developer.</p>
+          <p className="pb-8 text-lg">
+            I completed a six-month full-stack web development course where I
+            gained expertise in a range of technologies, including JavaScript, HTML, CSS, React, Git, Node/Express, and more. Feel free to check
+            out some of my projects below!
           </p>
-          <p className="pb-5 text-lg">
-            <span className="text-tang">Previously</span>, I had the opportunity to work alongside stakeholders from the public sector, 
-            including the German government, political foundations and NGOs, analyzing their digital communications and formulating 
-            effective strategies for improvement.
+
+          <p className="text-tang text-lg mb-3 flex">I have a background in digital communication.</p>
+          <p className="pb-8 text-lg">
+            As a consultant, I have worked for 6+ years with clients from the public sector, helping them enhance their digital communication efforts.
+            I have collaborated with teams of designers, developers and content creators to identify client needs
+            and devise strategies and products that elevate their digital presence.
           </p>
-          <p className="pb-5 text-lg">
-            <span className="text-tang">Recently</span>, I decided to pursue a dream of mine and enroll in CareerFoundry's Full-Stack Web
-            Development Program. Through this program, I have gained expertise in a range of technologies, including JavaScript, TypeScript, 
-            React, Angular, and more.
+
+          <p className="text-tang text-lg mb-3 flex">I am a lifelong learner and creative problem-solver.</p>
+          <p className="pb-8 text-lg">
+            Recently, I have transitioned into a more technical role driven by my passion for coding and creative problem-solving. I wanted to deepen my
+            understanding of web development and learn how to build robust products from the start.
           </p>
-          <p className="pb-5 text-lg">
-            <span className="text-tang">Now </span>I am enthusiastic about further expanding my software development skills and continuing my 
-            work as a detail-oriented developer with a comprehensive understanding of the development process.
+
+          <p className="text-tang text-lg mb-3 flex">I have a cross-functional skill set.</p>
+          <p className="text-lg">
+            By combining my expertise in digital communication and web development, I offer a versatile skill set that spans technical expertise,
+            analytical thinking and management skills. With this background, I am eager to co-create and implement new concepts
+            alongside a dynamic development team.
           </p>
         </div>
 
@@ -34,7 +42,7 @@ const About = () => {
           <img
             src={tagBracketsAbout}
             alt="placeholder-img"
-            className="mr-auto ml-auto mt-auto md:w-40 lg:w-60 xl:w-60 hidden lg:flex"
+            className="mr-auto ml-auto mt-auto lg:w-40 xl:w-60 hidden lg:flex"
           />
         </div>
       </div>
